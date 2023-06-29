c220525466527a1051620731890d17010517cc01
나 <hana825@naver.com>
2023-06-29 16:29:09 +0900

    Todo 추가

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,15 +8,20 @@ export default {
       newTodo: "",
       hideCompleted: false,
       todos: [
-        { id: id++, text: "React 분석하기" },
-        { id: id++, text: "Vue 배우기" },
+        { id: id++, text: "React 분석하기", done: true },
+        { id: id++, text: "Vue 배우기", done: false },
       ],
     };
+  },
+  computed: {
+    filteredTodos() {
+      return this.hideCompleted ? this.todos.filter((t) => !t.done) : this.todos;
+    },
   },
   methods: {
     addTodo() {
       if (this.newTodo.trim() != "") {
-        this.todos.push({ id: id++, text: this.newTodo });
+        this.todos.push({ id: id++, text: this.newTodo, done: false });
       } else alert("내용을 입력해주세요.");
       this.newTodo = "";
     },
@@ -29,17 +34,21 @@ export default {
 <template>
   <div class="todo">
     <form @submit.prevent="addTodo">
-      <input v-model="newTodo" />
+      <input type="text" v-model="newTodo" />
       <button>Add Todo</button>
     </form>
+    <button @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? "Show all" : "Hide completed" }}
+    </button>
     <ol>
-      <li v-for="todo in todos" :key="todo.id">
-        <input type="checkbox" v-model="todo.done" />
-        <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <li v-for="todo in filteredTodos" :key="todo.id">
+        <label
+          ><input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }">{{ todo.text }}</span>
+        </label>
         <button @click="removeTodo(todo)">X</button>
       </li>
     </ol>
-    <button @click="hideCompleted = !hideCompleted"></button>
   </div>
 </template>
 
@@ -55,6 +64,7 @@ export default {
     gap: 5px;
   }
   button {
+    margin-top: 10px;
     background-color: #f8f9fa;
     border: 1px solid #f8f9fa;
     border-radius: 4px;
@@ -68,17 +78,25 @@ export default {
     border: 1px solid #dadce0;
     color: #202124;
   }
-  input {
+  input[type="text"] {
     outline: none;
     border: 1px solid #dfe1e5;
     border-radius: 24px;
     min-height: 24px;
-    padding: 5px 8px 0 14px;
+    padding: 5px 8px 5px 14px;
   }
-  input:hover {
+  input[type="text"]:hover {
     background-color: #fff;
     box-shadow: 0 1px 6px rgba(32, 33, 36, 0.28);
     border-color: rgba(223, 225, 229, 0);
+  }
+  label,
+  input[type="checkbox"] {
+    cursor: pointer;
+  }
+  .done {
+    text-decoration: line-through;
+    color: #3c4043;
   }
 }
 </style>
