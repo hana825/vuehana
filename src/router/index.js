121d1b94ab9aa6087e14a09121cf92c0a8df0219
나 <hana825@naver.com>
2023-07-03 18:00:50 +0900

    url...

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Posts from "../views/Posts.vue";
 const router = createRouter({
   history: createWebHistory("/vuehana/"),
   // history: createWebHistory(),
+  // history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: "/",
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "./",
+  // publicPath: process.env.NODE_ENV === "production" ? "/vuehana/" : "/",
   chainWebpack: (config) => {
     config.module.rule("md").test(/\.md$/).use("raw-loader").loader("raw-loader").end();
   },
