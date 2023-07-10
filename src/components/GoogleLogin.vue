4e56f0bb03f3cb751a54e70981096a50117ae62e
나 <hana825@naver.com>
2023-07-10 15:16:30 +0900

    Update GoogleLogin.vue

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { decodeCredential } from "vue3-google-login";
 
 const callback = (response) => {
@@ -14,4 +14,32 @@ const callback = (response) => {
 
 <template>
   <GoogleLogin :callback="callback" prompt auto-login />
+</template> -->
+
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <GoogleLogin :callback="handleLogin" prompt="select_account" />
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { decodeCredential } from "vue3-google-login";
+
+const isLoggedIn = ref(false);
+
+const handleLogin = (response) => {
+  console.log("Login response:", response);
+
+  // 이미 로그인 되었는지 체크
+  if (response.isSignedIn) {
+    isLoggedIn.value = true;
+    const userData = decodeCredential(response.credential);
+    console.log("Decoded user data:", userData);
+    // 추가 처리 하거나 다른 페이지 이동..
+  } else {
+    isLoggedIn.value = false;
+  }
+};
+</script>
